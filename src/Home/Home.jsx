cdd87ed5919f7428dc1b4007731e2a3df266100c
justingreco <justin.greco@gmail.com>
2023-07-31 11:50:53 -0400

    updated paths to match production

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,12 +5,12 @@ import Tile from "../Tile/Tile";
 function Home() {
   return <>
   <div className="grid">
-  <Tile title="BUILDING PERMIT FEES" icon="./building.svg" path="buildings"></Tile>
+  <Tile title="BUILDING PERMIT FEES" icon="./building.svg" path="building"></Tile>
   <Tile title="STORMWATER FEES" icon="./stormwater.svg" path="stormwater"></Tile>
   <Tile title="OPEN SPACE FEES" icon="./openspace.svg" path="openspace"></Tile>
   <Tile title="THOROUGHFARE FEES" icon="./thoroughfare.svg" path="thoroughfare"></Tile>
   <Tile title="RIGHT-OF-WAY OCCUPANCY FEES" icon="./rightofway.svg" path="rightofway"></Tile>
-  <Tile title="RALEIGH WATER FEES" icon="./water.svg" path="water"></Tile>
+  <Tile title="RALEIGH WATER FEES" icon="./water.svg" path="raleighwater"></Tile>
   </div>
   </>;
 }

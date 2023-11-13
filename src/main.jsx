bd43f56061a92ce510c365bfcb0278d0f1926e6c
justingreco <justin.greco@gmail.com>
2023-11-13 18:06:43 -0500

    calculate fees on total valuation

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { setAssetPath } from "@esri/calcite-components/dist/components";
 setAssetPath(window.location.href);
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const baseUrl = '/permit-calculators-react/';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename={baseUrl}>
+    <BrowserRouter basename={baseUrl}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );

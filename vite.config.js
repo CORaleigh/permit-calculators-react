df2df0b68bdcd0affb1a81ad71193e794263ed53
justingreco <justin.greco@gmail.com>
2023-11-13 18:03:29 -0500

    calculate fees on total valuation

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 const config = {
-  base: '/permit-calculators/',
+  base: '/permit-calculators-react/',
   plugins: [react()], 
   build: {
     chunkSizeWarningLimit: 2000,

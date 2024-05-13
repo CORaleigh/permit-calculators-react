f62d63eeccc5f54d5b6a48486d024cb0504ca507
justingreco <justin.greco@gmail.com>
2024-05-13 08:53:25 -0400

    updated open space fees for FY24/25

--- src/OpenSpace/openSpaceConfig.js
+++ src/OpenSpace/openSpaceConfig.js
@@ -1,20 +1,20 @@
 export const zones = [{
   zone: 1,
-  single: 1794,
-  multi: 1302
+  single: 1842,
+  multi: 1337
 },
 {
   zone: 2,
-  single: 1794,
-  multi: 1302
+  single: 1842,
+  multi: 1337
 },
 {
   zone: 3,
-  single: 1511,
-  multi: 1099
+  single: 1552,
+  multi: 1129
 },
 {
   zone: 4,
-  single: 1517,
-  multi: 1114
+  single: 1558,
+  multi: 1144
 }];
